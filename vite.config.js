08b3e33d5383707043c0546492cc71bba68f2ff4
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-08-02 12:03:07 +0200

    Fix vite build for fortawesome icons

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	// Temporary vite workaround for fortawesome icons
+	// see https://github.com/josdejong/svelte-jsoneditor/issues/107
+	ssr: {
+		noExternal: [
+		  "@fortawesome/*"
+		]
+	  }
 };
 
 export default config;
