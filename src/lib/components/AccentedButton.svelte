0eefc040b8ec863c8a3bb62e9e8e6422d70cc2de
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2023-07-24 17:47:42 +0200

    Update version

--- src/lib/components/AccentedButton.svelte
+++ src/lib/components/AccentedButton.svelte
@@ -2,7 +2,7 @@
 	export let href, text;
 </script>
 
-<a sveltekit:prefetch {href}>
+<a data-sveltekit-preload-data="hover" {href}>
 	<button
 		class="border-2 pb-2 pt-1 px-4 font-semibold text-2xl text-accent border-accent hover:text-white hover:bg-accent transition"
 	>
